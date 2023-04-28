0c78656d8fa2c15a4543d06d3cde31f84649bca3
John <johnkaranjairungu@gmail.com>
2023-04-28 23:57:11 +0300

    added a useEffect hook

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function QuestionList() {
+  const [items, setItems] = useState([]);
+
+  // using useEffect hook to render list on loading
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((items) => console.log(items));
+  }, []);
+
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{}</ul>
     </section>
   );
 }
